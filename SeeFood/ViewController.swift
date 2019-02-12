30ec5e41ba848e1cc92a4f304b513be1e96866a7
Niraj Tiwari <tniraj7@gmail.com>
2019-02-12 15:18:06 +0900

    * Added camera action to action sheet * Added try again button

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -25,6 +25,9 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         displayActionSheet(self)
     }
     
+
+    @IBOutlet weak var tryAgainButton: UIButton!
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
         if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
@@ -35,6 +38,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             }
             
             detect(image: ciimage)
+            displayActionSheet(self)
         }
         imagePicker.dismiss(animated: true, completion: nil)
         
@@ -71,13 +75,25 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     func displayActionSheet(_ sender: Any) {
-        let optionsMenu = UIAlertController(title: nil, message: "Choose Option", preferredStyle: .actionSheet)
+        let optionsMenu = UIAlertController(title: "Choose an image source", message: nil, preferredStyle: .actionSheet)
         
-        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
-        let libraryAction = UIAlertAction(title: " Choose from Library", style: .default)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { ACTION in
+            optionsMenu.dismiss(animated: true)
+        }
         
-        optionsMenu.addAction(cancelAction)
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) { ACTION in
+            self.imagePicker.sourceType = .camera
+            self.present(self.imagePicker, animated: true, completion: nil)
+        }
+        
+        let libraryAction = UIAlertAction(title: " Choose from Library", style: .default) { ACTION in
+            self.imagePicker.sourceType = .photoLibrary
+            self.present(self.imagePicker, animated: true, completion: nil)
+        }
+        
+        optionsMenu.addAction(cameraAction)
         optionsMenu.addAction(libraryAction)
+        optionsMenu.addAction(cancelAction)
         
         self.present(optionsMenu, animated: true, completion: nil)
     }
